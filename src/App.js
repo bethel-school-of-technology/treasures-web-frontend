b6f541501e0a687d3478b73c0317a00a9973b925
Kenton Kiley <stonehartk3@gmail.com>
2020-11-23 17:46:35 -0500

    Added some more words to the welcome page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the Treasures New Website!
+          Welcome to the Treasures New Website! Hope you enjoy your stay!
         </p>
         <a
           className="App-link"
