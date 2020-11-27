f67a8ffdcc8f0e46be16931699032afec68ab346
jmz2116 <jzaver2116@gmail.com>
2020-11-27 14:52:54 -0600

    "Added Footer.js to footer folder but need to make more changes also added script src in index.html"

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer class="footer">
+                <div class="container">
+                    <span class="text-muted"> Place footer content here</span>
+                </div>
+            </footer>
+        )
+    }
+}
