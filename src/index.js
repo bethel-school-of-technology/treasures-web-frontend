08a32b13342361bcfea491f2e6683d6f03daebac
Kenton Kiley <stonehartk3@gmail.com>
2020-11-27 16:36:00 -0500

    Got pages to load properly, added a css for ease of viewing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import MyForm from './pages/contact/contact';
+import Contact from './pages/contact/contact';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Pages = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home Page</Link></li>
+        <li><Link to="/Contact">Contact Us</Link></li>
+      </ul>
+      <hr />
+      <Route exact path="/" component={App} />
+      <Route path="/Contact" component={Contact} />
+    </div>
+  </Router>
 );
+
+ReactDOM.render(<Pages />, document.getElementById('root'));
