f71db49e35326cf0569ae7ee492068fe981e170d
Kristin Terrazas <kristinterrazas@Kristins-iMac.local>
2020-12-03 11:12:05 +0000

    adding bootstrap - nav started

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,44 +1,43 @@
 import React from "react";
-import './Footer.css';
 
 const currentDate = new Date();
 const year = currentDate.getFullYear();
 
 class Footer extends React.Component {
-    render() {
-        return (
-          
-          <footer id="footer">
-          <div className="container">
-            <div className="row">
-              <div className="logo-footer">Treasures Website</div>
-              <div className="info">
-                <p>Location: United Kingdom</p>
-                <p>Contact: (888)123-4567</p>
-              </div>
-              <div className="social">
-                <a href="/" target="_blank">
-                  <i className="fa fa-facebook"></i>
-                </a>
-                <a href="/" target="_blank">
-                  <i className="fa fa-instagram"></i>
-                </a>
-                <a href="/" target="_blank">
-                  <i className="fa fa-twitter"></i>
-                </a>
-              </div>
-              <div className="copyright">
-                <p>
-                  <span dangerouslySetInnerHTML={{ __html: "&copy;" }} />
+  render() {
+    return (
+
+      <footer id="footer">
+        <div className="container">
+          <div className="row">
+            <div className="logo-footer">Treasures Website</div>
+            <div className="info">
+              <p>Location: United Kingdom</p>
+              <p>Contact: (888)123-4567</p>
+            </div>
+            <div className="social">
+              <a href="/" target="_blank">
+                <i className="fa fa-facebook"></i>
+              </a>
+              <a href="/" target="_blank">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="/" target="_blank">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </div>
+            <div className="copyright">
+              <p>
+                <span dangerouslySetInnerHTML={{ __html: "&copy;" }} />
                   Treasures Ministries {year}
-                </p>
-              </div>
+              </p>
             </div>
           </div>
-        </footer>
-      
-        );
-    }
+        </div>
+      </footer>
+
+    );
+  }
 }
 
 export default Footer;
