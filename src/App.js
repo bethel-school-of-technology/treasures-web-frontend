9efa7779bce76779f00cbf6c1f86c30be3e49384
jmz2116 <jzaver2116@gmail.com>
2020-12-05 14:31:25 -0600

    Changed footer grid/bootstrap things, changed a bit of text in App.js, WIP on footer

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <br></br>
         <hr></hr>
         <br></br>
-        <h3>Thank you for coming by to say hello…!!! Our heart is that you will feel like you’ve just popped into our home for coffee… that you will be wonderfully inspired and feel extravagantly loved as you find out a little more about us…</h3>
+        <h3>Thank you for coming by to say hello!! Our heart is that you will feel like you’ve just popped into our home for coffee, that you will be wonderfully inspired, and feel extravagantly loved as you find out a little more about us.</h3>
       </header>
     </div>
   ); 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const currentDate = new Date();
 const year = currentDate.getFullYear();
@@ -6,38 +7,55 @@ const year = currentDate.getFullYear();
 class Footer extends React.Component {
   render() {
     return (
+      <footer class="footer">
+        <div class="container">
+          <div class="row row-cols-3 order-first">
+            <div class="footer-col col-lg-3">
+              <h3 class="col-title">About</h3>
+              <ul class="footer-menu list-unstyled">
+                <li>
+                  <Link class="text-link" href="#">
+                    Our Heart
+                  </Link>
+                </li>
+                <li>
+                  <Link class="text-link" href="#">
+                    Resources
+                  </Link>
+                </li>
+                <li>
+                  <Link class="text-link" href="#">
+                    Declarations
+                  </Link>{" "}
+                  <span class="badge md-info alert">Download</span>
+                </li>
+              </ul>
+            </div>
 
-      <footer id="footer">
-        <div className="container">
-          <div className="row">
-            <div className="logo-footer">Treasures Website</div>
-            <div className="info">
-              <p>Location: United Kingdom</p>
-              <p>Contact: (888)123-4567</p>
+            <div class="footer-col col-lg-3">
+              <div class="divider"></div>
+              <br />
+              <br />
+              <small class="copyright">Treasures @ {year} </small>
             </div>
-            <div className="social">
-              <a href="/" target="_blank">
-                <i className="fa fa-facebook"></i>
-              </a>
-              <a href="/" target="_blank">
-                <i className="fa fa-instagram"></i>
-              </a>
-              <a href="/" target="_blank">
-                <i className="fa fa-twitter"></i>
-              </a>
-            </div>
-            <div className="copyright">
-              <p>
-                <span dangerouslySetInnerHTML={{ __html: "&copy;" }} />
-                  Treasures Ministries {year}
-              </p>
+            <div class="footer-col col-lg-3 order-last">
+              <h3 class="col-title">Contact</h3>
+              <ul class="footer-menu list-unstyled">
+                <li>
+                  <Link class="text-link" href="#">
+                    Contact Us
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
       </footer>
-
     );
   }
 }
 
 export default Footer;
+{
+  /* <li><Link class="text-link" href="#">Contact Us</Link></li> */
+}
