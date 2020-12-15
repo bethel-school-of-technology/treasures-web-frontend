0224f12e5acac4e3b774be9ed22d09b768ab72d0
Peter <peter.waller53@gmail.com>
2020-12-15 00:09:02 +0000

    Updated basic data on Declarations, Our Hearts and Mentoring

--- src/pages/resources/resources.js
+++ src/pages/resources/resources.js
@@ -45,19 +45,19 @@ function Resources() {
   };
 
   return (
-    <div className="resourcesContainer">
+    <div className="resources">
       <h2>Resources</h2>
       <p>We currently have a number of really amazing and lovely  journals, all written and designed by Karen (20cm square in size). All just £10 each, plus postage (£2 per journal) or free collection. To keep costs low we ask you to simply email us below the Journals you would like to purchase and your postage address and we will email you the payment information and post it to you… simple!</p>
 
 
-      <h3>Looping through the cardRender</h3>
+      <h2>Looping through the cardRender</h2>
       {cardInfo.map(renderCard)}
 
 
       <hr></hr>
 
       <div className="container-fluid">
-        <h3>Contact us directly to purchase these Journals</h3>
+        <h2>Contact us directly to purchase these Journals</h2>
 
         {/* <div className="row justify-content-around"> */}
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 justify-content-around">
@@ -150,7 +150,7 @@ function Resources() {
       <hr></hr>
 
       <div className="container-fluid">
-        <h3>Books that are available on Amazon</h3>
+        <h2>Books that are available on Amazon</h2>
 
         <div className="row justify-content-around bookSelf">
           <div className="col-6 col-md-4">
@@ -159,7 +159,7 @@ function Resources() {
             </a>
           </div>
           <div className="col-12 col-md-8">
-            <h4>Encounters</h4>
+            <h3>Encounters</h3>
             <p >Just one short encounter with Jesus was all it took for people to step into freedom, wholeness and life, even after years of suffering with complex issues. Yet each encounter was the result of a journey. What was going on in their hearts and minds as they reached out to Jesus? What were their hopes and fears as they journeyed towards Him? - Maybe they were the same ones as you have?</p>
             <p>These ten stories are based on the accounts found in the gospels but they are not meant to be read as scripture. However, they are written to be consistent with both God’s and man’s nature. By dramatising the accounts, my hope is that they would help you experience just one possibility of what it might have been like for each person. A glimpse into the unique journey of their hearts and minds, of their hopes and fears, leading up to the moment of their encounter with Jesus and subsequent breakthrough. Every story, every breakthrough, has been recorded in scripture to encourage us. For what God has done for one, is available for all… for you!</p>
             <p>After each short story I have added a few thoughts and questions to help you ponder, explore and go on your own journey encountering Jesus.</p>
@@ -174,7 +174,7 @@ function Resources() {
             </a>
           </div>
           <div className="col-12 col-md-8">
-            <h4>Who Am I? &amp; Who Are You?</h4>
+            <h3>Who Am I? &amp; Who Are You?</h3>
             <p>This book is not simply a book... it’s an adventure!</p>
             <p>A treasure map to discovering the gold that is hidden inside yourself and all those amazing people around you! God sees the gold and the goodness in YOU and in every single person in your world!</p>
             <p>So here begins the adventure... an adventure you can go on by yourself, with friends and family or in a group whatever age you all are, isn’t that cool! An adventure that will help you to see the Bible and hear God’s voice in a new way. An adventure that will take you through all seven continents of the world as we explore lots of different animals, enjoy fun and creative activities and learn many ways to discover the gold in ourselves and others!</p>
