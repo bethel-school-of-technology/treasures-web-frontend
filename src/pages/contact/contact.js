b4b5b010ae78a7e218761a1cfd6b2ea9f3ffcd2a
Kenton Kiley <stonehartk3@gmail.com>
2020-12-17 12:24:37 -0500

    Added in more on the form for contacts, also added in axios

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import './contact.css';
+import axios from 'axios';
 
 
 class MyForm extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            name: '',
+            email: '',
+            message: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    async handleSubmit(e) {
+        e.preventDefault()
+
+        const { name, email, message } = this.state
+
+        const form = await axios.post('/contacts/api/form', {
+            name,
+            email,
+            message
+        })
+    }
+
     render() {
         return (
             <div className="center">
