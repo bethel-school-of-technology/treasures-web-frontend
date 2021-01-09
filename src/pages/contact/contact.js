b855c655b6b185ede5f899187a5411a1ac8e7130
Kenton Kiley <kentonkiley@Kentons-MacBook-Pro.local>
2021-01-09 09:43:53 -0500

    commented out the old, put in the new code for email to work

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './contact.css';
-import axios from 'axios';
+//import axios from 'axios';
+
+function MyForm() {
+
+    var [nick, setNick] = useState("");
+    var [email, setEmail] = useState("");
+    var [message, setMessage] = useState("");
 
 
-class MyForm extends React.Component {
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        let newContact = await fetch("http://localhost:4000/contacts/api/form", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ nick, email, message })
+        })
+        let newContactForm = newContact.json();
+        console.log(newContactForm);
+    }
+
+    return (
+        <div className="center">
+            <form onSubmit={handleSubmit}>
+                <label>Name:</label><br />
+                <input type="text" name="identity" onChange={e => setNick(e.target.value)}></input><br />
+                <label>Email Address:</label><br />
+                <input type="text" name="email" onChange={e => setEmail(e.target.value)}></input><br />
+                <label>Questions or Comments:</label><br />
+                <textarea placeholder="Please type in here." name="message" onChange={e => setMessage(e.target.value)} cols="50" rows="10"></textarea><br />
+                <input class="submit-button" type="submit" name="send" value="Submit"></input>
+            </form>
+        </div>
+    )
+}
+
+/*class MyForm extends React.Component {
     constructor() {
         super()
 
@@ -48,6 +80,6 @@ class MyForm extends React.Component {
             </div>
         )
     }
-}
+}*/
 
 export default MyForm;
